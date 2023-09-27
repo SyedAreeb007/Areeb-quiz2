0acc87225300248d73ac3844917cd105013a8804
SyedAreeb007 <144684190+SyedAreeb007@users.noreply.github.com>
2023-09-27 13:12:09 +0500

    fourth commit

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -8,42 +8,36 @@ function abc(num,amount)
 
 console.log(abc(50,10000))
 
-//task 2
+// Task 2
 const people = [
     {
-        id: 1
-      personName: "Fahad",
-      personAge: 23,
-      personIncome: "2000000"
+        id: 1,
+        personName: "Fahad",
+        personAge: 23,
+        personIncome: "2000000"
     },
     {
-        id: 2
+        id: 2,
         personName: "Zoya",
         personAge: 23,
-        personIncome: "3000000",
+        personIncome: "3000000"
     }
-    
-  ];
-  
-  
-  // Print the names of all people in the array
+];
+
 console.log("Names of all people:");
 people.forEach((person) => {
-  console.log(person.name);
+  console.log("Person ",person.id, "name is ",person.personName," having salary ",person.personIncome);
 });
 
-// Print the ages of all people in the array
-console.log("\nAges of all people:");
+//task 03
+  console.log("Properties of the objects:");
 people.forEach((person) => {
-  console.log(person.age);
+  for (const property in person) {
+    console.log(property);
+  }
 });
 
-// Print the cities of all people in the array
-console.log("\nCities of all people:");
-people.forEach((person) => {
-  console.log(person.city);
-});
-
+  
 
 
 
