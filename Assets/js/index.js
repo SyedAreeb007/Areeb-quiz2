911453b7c2fab4d5daefc2d6317d87dd0f48ea1e
MSaifChanna <sacthegame1@gmail.com>
2023-09-27 12:56:24 +0500

    second commit

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -7,3 +7,44 @@ function abc(num,amount)
 }
 
 console.log(abc(50,10000))
+
+//task 2
+const people = [
+    {
+        id: 1
+      personName: "Fahad",
+      personAge: 23,
+      personIncome: "2000000"
+    },
+    {
+        id: 2
+        personName: "Zoya",
+        personAge: 23,
+        personIncome: "3000000",
+    }
+    
+  ];
+  
+  
+  // Print the names of all people in the array
+console.log("Names of all people:");
+people.forEach((person) => {
+  console.log(person.name);
+});
+
+// Print the ages of all people in the array
+console.log("\nAges of all people:");
+people.forEach((person) => {
+  console.log(person.age);
+});
+
+// Print the cities of all people in the array
+console.log("\nCities of all people:");
+people.forEach((person) => {
+  console.log(person.city);
+});
+
+
+
+
+
